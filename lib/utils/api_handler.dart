fe47aedf991e658db353724153b89bf3b63d0674
Nevena Dukic <dukicn01@gmail.com>
2025-07-27 21:47:59 +0200

    Admin Screen

--- lib/utils/api_handler.dart
+++ lib/utils/api_handler.dart
@@ -1,5 +1,6 @@
 class BaseAPI {
   static const String ipPort = "10.0.2.2:7039";
+  //static const String ipPort = "192.168.1.28:7039";
   static const String base = "https://$ipPort";
 
   static const Map<String, String> defaultHeaders = {
